52b45feb8c991c33f5cdfe76a841096ed7cb3a57
Boloxxx <bryanrodriguezalmonte@gmail.com>
2024-08-22 16:24:08 -0400

    v.04

---
+++ app/(routes)/(home)/components/HeaderMain/HeaderMain.types.ts
@@ -0,0 +1,9 @@
+type TypeElement = "" | "password" | "folder";
+
+export type DataHeaderMainItemProps = {
+  icon: React.ComponentType<{ className?: string }>;
+  typeElement: TypeElement;
+  text: string;
+};
+
+
